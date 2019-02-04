c07e1a0f9c70b21ddf8623def9f8838055ca8658
Anna <annaecataldo@gmail.com>
2019-02-04 15:02:51 -0500

    MUD story edits

--- MUD/practice.js
+++ MUD/practice.js
@@ -56,21 +56,21 @@ function engage() {
 	if (currentLocation == "s_gate") {
 			$("<p>The southern gate of the Castle looms above you. The wind is quiet and no danger is in sight. MOVE NORTH to reach the Castle entrance, and keep your sword close.</p>").insertBefore("#placeholder").fadeIn(1000);
 		} else if (currentLocation == "s_west") {
-			$("<p>The southwestern corner of this realm is known for its attention-seeking, and sometimes vengeful spirits. A piercing wail strikes the wind and your heart seizes. A shadow whips around you. Legend has it that the spirits in this realm are afraid of nothing but their own reflection. With stiff limbs you reach into your satchel praying to grasp a mirror.</p>").insertBefore("#placeholder").fadeIn(1000);
+			$("<p>The southwestern corner of this realm is known for its vengeful spirits. A wail strikes the wind and your heart seizes. A shadow whips around you. Stories say the spirits in this realm are afraid of nothing but their own reflection. With stiff limbs you reach into your satchel praying to find a mirror.</p>").insertBefore("#placeholder").fadeIn(1000);
 		} else if (currentLocation == "s_east") {
-			$("<p>The southeastern corner is home to the humans of this realm. You hear the sounds of industrious life all around you, and long for the family you left behind.</p>").insertBefore("#placeholder").fadeIn(1000);
+			$("<p>The southeastern corner is home to the humans of this realm. You hear the sounds of industrious life all around you, and long for the home you left behind.</p>").insertBefore("#placeholder").fadeIn(1000);
 		} else if (currentLocation == "castle"){
-			$("<p>The Castle is more brilliant than you could have imagined. The entrance is elaborately carved with the curiosities native to this realm - Spirits, ogres, and treasures in great bounty. More than anything, you wish to enter, but the door is locked.</p>").insertBefore("#placeholder").fadeIn(1000);
+			$("<p>The Castle is more remarkable than you could have imagined. The entrance is finely carved with curiosities of the realm - Spirits, ogres, and treasures you've only heard of in legend. You wish to enter, but the door is locked.</p>").insertBefore("#placeholder").fadeIn(1000);
 		} else if (currentLocation == "west") {
-			$("<p>The day is at its end and the western realm is dark. Your surroundings are obscured by the setting sun, leaving you vulnerable to creatures unknown.</p>").insertBefore("#placeholder").fadeIn(1000);
+			$("<p>The day is at its end and the western realm is dark. Your surroundings are blurred by the setting sun, leaving you vulnerable to creatures unknown.</p>").insertBefore("#placeholder").fadeIn(1000);
 		} else if (currentLocation == "east") {
-			$("<p>The eastern sunrise is exquisite, but your hunger is consuming you.</p>").insertBefore("#placeholder").fadeIn(1000);
+			$("<p>The eastern sunrise is stunning, but your hunger is consuming you.</p>").insertBefore("#placeholder").fadeIn(1000);
 		} else if (currentLocation == "n_west") {
-			$("<p>The northwestern corner of this realm is marked with bare treasure coves, all raided long ago by creatures seeking great riches and power. Ogres have made this their home, hostile beasts motivated by hunger and greed. The ground shakes beneath you, and you know an ogre has caught your scent. You have three choices: Try to strike him with your sword, pierce his eyes with your dagger, or buy his mercy with your ruby...</p>").insertBefore("#placeholder").fadeIn(1000);
+			$("<p>The northwestern corner of this realm is marked with bare treasure coves, all raided long ago by creatures seeking riches. It is now home to the ogres, hostile beasts motivated by hunger and greed. The ground shakes beneath you, and you know an ogre has caught your scent. You have three choices: Try to strike him with your sword, pierce his eyes with your dagger, or buy his mercy with your ruby...</p>").insertBefore("#placeholder").fadeIn(1000);
 		} else if (currentLocation == "n_gate") {
-			$("<p>You have strayed far from your entry into this realm, and you feel your journey coming to an end. Your thirst is as acute as your desire for a long-awaited safe haven, and you search around your satchel for something to drink.</p>").insertBefore("#placeholder").fadeIn(1000);
+			$("<p>You have strayed far from your entry into this realm, and you feel your journey coming to an end. Your thirst is as acute as your desire for a safe haven, and you search in your satchel for something to drink.</p>").insertBefore("#placeholder").fadeIn(1000);
 		} else if (currentLocation == "n_east") {
-			$("<p>The northeastern corner of this realm is life-threateningly cold. You've heard tales of the dead trapped here, eternally searching for warmth, frozen from within. One such ice creature is making its way towards you in the distance. You have time for just one action: wrap yourself in a warm layer to save yourself from a frozen fate, or brandish your sword against the enemy.</p>")
+			$("<p>The northeastern corner of this realm is deathly cold. You've heard tales of the ghosts trapped here, frozen from the inside out and eternally searching for the warmth of the living. One such ice creature is making its way towards you in the distance. You have time for just one move: wrap yourself in a warm layer to save yourself from freezing, or brandish your sword against the enemy.</p>")
 		}
 }
 
@@ -139,7 +139,7 @@ function pickUpSword() {
 	if (currentLocation == "s_gate") {
 			newPlayer.inventory.push("Sword");
 			updateInventory();
-			$("<p>You picked up the sword. The weight of the blade feels powerful, and you are emboldened to explore the world around you. Move NORTH, SOUTH, EAST, or WEST.</p>").insertBefore("#placeholder").fadeIn(1000);
+			$("<p>You picked up the sword. The weight of it feels powerful, and you are emboldened to explore the world around you. Move NORTH, SOUTH, EAST, or WEST.</p>").insertBefore("#placeholder").fadeIn(1000);
 		} else {
 			$("<p>The sword is not here. Proceed to the South Gate to find the sword, or continue to explore.</p>").insertBefore("#placeholder");
 		}
@@ -181,7 +181,7 @@ function pickUpDagger() {
 		updateInventory();	
 		$("<p>You picked up the dagger.</p>").hide().insertBefore("#placeholder").fadeIn(1000);
 	} else {
-		$("<p>The dagger is not here. Proceed to the southeast for a pocket weapon, or continue to explore.</p>").insertBefore("#placeholder");
+		$("<p>The dagger is not here. Proceed to the southeast for a weapon, or continue to explore.</p>").insertBefore("#placeholder");
 	}
 }
 
@@ -199,7 +199,7 @@ function pickUpBread() {
 	if(currentLocation == "east") {
 		newPlayer.inventory.push("Bread");
 		updateInventory();
-		$("<p>You picked up the bread, consumed by hunger.</p>").hide().insertBefore("#placeholder").fadeIn(1000);
+		$("<p>You picked up the bread, weak with hunger.</p>").hide().insertBefore("#placeholder").fadeIn(1000);
 	} else {
 		$("<p>The bread is not here. Proceed to the East to find food, or continue to explore.</p>").insertBefore("#placeholder");
 	}	
@@ -244,7 +244,7 @@ function useSword() {
 	removeItem(newPlayer.inventory, "Sword");
 	updateInventory();
 	console.log(newPlayer.inventory);
-	$("<p>You wield your sword against your fearsome enemy. The blade strikes him and he slows, but does not die. Run!</p>").insertBefore("#placeholder").fadeIn(1000);
+	$("<p>You wield your sword against your enemy. The blade strikes him and he slows, but does not die. Run!</p>").insertBefore("#placeholder").fadeIn(1000);
 }
 
 function useTorch() {
@@ -256,19 +256,19 @@ function useTorch() {
 function useCloak() {
 	removeItem(newPlayer.inventory, "Cloak");
 	updateInventory();
-	$("<p>You pull the cloak from your satchel and wrap it around your body. You can instantly feel your strength return.</p>").insertBefore("#placeholder").fadeIn(1000);
+	$("<p>You pull the cloak from your satchel and wrap it around your body. You can feel your strength return.</p>").insertBefore("#placeholder").fadeIn(1000);
 }
 
 function useKey() {
 	removeItem(newPlayer.inventory, "Key");
 	updateInventory();
-	$("<p>You grasp the heavy key in both hands and turn it in the expertly crafted Castle lock. It clicks open, you enter. A castle guard is stationed directly inside, poised for battle. You have three choices, run, bribe, or fight.</p>").insertBefore("#placeholder").fadeIn(1000);
+	$("<p>You grasp the heavy key in both hands and turn it in the expertly crafted lock. It clicks open, you enter. A castle guard is stationed directly inside, poised for battle. You have three choices, run, bribe, or fight.</p>").insertBefore("#placeholder").fadeIn(1000);
 }
 
 function useDagger() {
 	removeItem(newPlayer.inventory, "Dagger");
 	updateInventory();
-	$("<p>You grip the dagger and bury it into your enemy's good eye. He bellows in pain, and crumbles to the ground, clutching his face. You have time to escape!</p>").insertBefore("#placeholder").fadeIn(1000);
+	$("<p>You grip the dagger and bury it into your enemy's good eye. He bellows in pain, and falls to the ground, clutching his face. You have time to escape!</p>").insertBefore("#placeholder").fadeIn(1000);
 }
 
 function useRuby() {
@@ -286,13 +286,13 @@ function  useBread() {
 function useWine() {
 	removeItem(newPlayer.inventory, "Wine");
 	updateInventory();
-	$("<p>You gulp your wine with great fervor. Continue to explore.</p>").insertBefore("#placeholder").fadeIn(1000);
+	$("<p>You drink your wine with relief. Continue to explore.</p>").insertBefore("#placeholder").fadeIn(1000);
 }
 
 function useMirror() {
 	removeItem(newPlayer.inventory, "Mirror");
 	updateInventory();
-	$("<p>You pull the mirror from your satchel and thrust it towards the spirit. It shrieks at the sight of its own reflection and retreats, leaving a thin veil of soot in its path.</p>").insertBefore("#placeholder").fadeIn(1000);
+	$("<p>You pull the mirror from your satchel and thrust it towards the spirit. It shrieks at the sight of its own reflection and retreats, leaving a trail of dust in its path.</p>").insertBefore("#placeholder").fadeIn(1000);
 }
 
 
@@ -362,15 +362,15 @@ $(document).ready(function () {
 
 		////NAVIGATE MAP////
 
-		var arriveNorthwest = "<p>You are now at the northwestern corner of this world. You've heard legends of treasures in this realm, and peel your eyes for riches among the muted colors of the earth around you. Against all odds, a faint red glow is flickering against a tree root nearby. A ruby!</p>";
-		arriveNorthGate = "<p>You are now at the northern gate of the Castle. Your journey has made you thirsty. A wine cellar extends beyond the northern Castle grounds, and you see an unattended wagon stacked with small flasks of wine.</p>";
-		arriveNortheast = "<p>You are now at northeastern corner of this world. There is a chill in the north, and you see a cast-off cloak lining the road.</p>";
+		var arriveNorthwest = "<p>You are now at the northwestern corner of this world. You've heard legends of treasures in this realm, and peel your eyes for riches around you. Against all odds, a faint red glow is flickering against a tree root nearby. A ruby!</p>";
+		arriveNorthGate = "<p>You are now at the northern gate of the Castle. Your journey has made you thirsty. A wine cellar is beyond the northern Castle grounds, and you see an unattended wagon stacked with flasks of wine.</p>";
+		arriveNortheast = "<p>You are now at northeastern corner of this world. There is a chill in the north, and you see a cast-off cloak waiting for you the road.</p>";
 		arriveWest = "<p>You are now at the western most territory of this world. The sun is setting, and you find a torch to light the way.</p>";
-		arriveCastle = "<p>You are now at the foot of the Castle. You feel a fleeting pull at your hem and turn around to find someone has left an intricate key at your heels.</p>";
-		arriveEast = "<p>You are now at the eastern most territory of this world. The sun is rising, and you feel the faint ache of hunger. A baker in the East Market is giving away last week's bread, and the stale yeasty scent is drawing you in.</p>";
-		arriveSouthwest = "<p>You are now at the southwestern corner of this world. The sun catches the surface of a small, ornamented mirror on your path.</p>";
+		arriveCastle = "<p>You are now at the foot of the Castle. You feel a fleeting pull at your hem and turn around to find someone has left an intricate key at your feet.</p>";
+		arriveEast = "<p>You are now at the eastern most territory of this world. The sun is rising, and you feel an aching hunger. A baker in the East Market is giving away last week's bread, and the stale scent draws you there.</p>";
+		arriveSouthwest = "<p>You are now at the southwestern corner of this world. The sun catches the surface of a small, flickering mirror on your path.</p>";
 		arriveSouthGate = "<p>You are now at the Southern Gate of the Castle grounds. You see a glint of light obscured in the soil and crouch towards it, wiping away the leaves and earth to find ... a sword!</p>";
-		arriveSoutheast = "<p>You are now at the southeastern corner of this world. In a clearing you see a young girl pantomiming a knight in battle, impossibly light on her feet. She is clutching a small hunting dagger in her hand, which she drops and kicks away at the sound of her mother's voice calling her home.</p>";
+		arriveSoutheast = "<p>You are now at the southeastern corner of this world. In a clearing you see a young girl pantomiming a knight in battle. She is clutching a small hunting dagger in her hand, which she drops and kicks away at the sound of someone calling her home.</p>";
 		outerLimits = "<p>You've reached the outer limits of this world. Explore in another direction.</p>";
 
 
